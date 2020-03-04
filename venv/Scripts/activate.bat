31a1bfc51f016beaf54dec1544cd90553da08022
Eyong Egbe <lambert.egbe@yahoo.co.uk>
2020-03-04 21:50:42 +0000

    missing files added

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\AutomationWithPython\AutomatedTesting\reqres-api-tests\venv
+set VIRTUAL_ENV=C:\Users\eyongegbe\PycharmProjects\reqres-api-tests\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
--- venv/Scripts/pip3-script.py
+++ venv/Scripts/pip3-script.py
@@ -1,4 +1,4 @@
-#!C:\AutomationWithPython\AutomatedTesting\reqres-api-tests\venv\Scripts\python.exe
+#!C:\Users\eyongegbe\PycharmProjects\reqres-api-tests\venv\Scripts\python.exe
 # EASY-INSTALL-ENTRY-SCRIPT: 'pip==19.0.3','console_scripts','pip3'
 __requires__ = 'pip==19.0.3'
 import re
